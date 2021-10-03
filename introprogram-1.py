8adeaf4db5f8f5b7af02618df4653b894493d67c
divya072899 <90974654+divya072899@users.noreply.github.com>
2021-10-03 19:21:10 +0530

    Intro program-0

---
+++ introprogram-1.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on sat sep 18 15:07:19 2021
+
+@author: pc
+"""
+Aug_batch = [1,2,3,4,5,6,7,8]
+print(Aug_batch)
+Aug_batch
+import matplotlib.pyplot as pt
+pt.plot(Aug_batch)
+
